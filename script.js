0ef3681001e78d8e49920113493fefa7e072a1f0
Raghavvendra Rai K <raghavendrarai.kadar@gmail.com>
2018-11-25 23:22:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var app = angular.module("myApp",[]);
 
 app.controller("sliderController",function($scope){
-	$scope.$timeperiodData ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"];
+	$scope.timeperiodData = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"];
 	
-	$scope.timeperiodChanged = function (timeperiodData, leftIndex, rightIndex, parents, isTrend) {
-		console.log(timeperiodData[leftIndex] + (isTrend? " - " + timeperiodData[rightIndex] :""));
+	$scope.timeperiodChanged = function (timeperiodData, leftIndex, rightIndex) {
+		console.log(timeperiodData[leftIndex] + (rightIndex == undefined ? " - " + timeperiodData[rightIndex] :""));
 	};
 	
 	$scope.getTimeperiodText = function (timeperiodData,index){
